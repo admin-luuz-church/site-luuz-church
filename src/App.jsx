b819611ca8ff8fa06a551e7b6a55b94d9ad73cd7
admin-luuz-church <admin@luuz.com.br>
2025-08-05 10:24:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function Location() {
       <p className="text-lg mb-4">Rua Doutor Bueno, 251 – Macaé, RJ</p>
       <iframe
         className="w-full max-w-3xl h-80 mx-auto rounded-xl shadow-md"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.3829449683913!2d-41.765000485021736!3d-22.490503136647794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x98ab5f5d14d555%3A0x25071d84138c5db!2sR.%20Dr.%20Bueno%2C%20251%20-%20Centro%2C%20Maca%C3%A9%20-%20RJ!5e0!3m2!1spt-BR!2sbr!4v1692040303512!5m2!1spt-BR!2sbr"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3688.7317197601164!2d-42.98088792546848!3d-22.401469020005653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x984cdfaaf4bcad%3A0x110101d7b16587c0!2sR.%20Manoel%20Dias%2C%20291%20-%20Barra%20do%20Imbu%C3%AD%2C%20Teres%C3%B3polis%20-%20RJ%2C%2025965-660!5e0!3m2!1spt-BR!2sbr!4v1754399992126!5m2!1spt-BR!2sbr"
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
         allowFullScreen
